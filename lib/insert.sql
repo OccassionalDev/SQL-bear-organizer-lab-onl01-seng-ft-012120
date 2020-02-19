be5d0cd6461c7e05c920ec2f358428a68eb1f50d
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-19 16:40:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy'
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 16, 'F', 'Gentle')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 27, 'M', 'Adamant')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 11, 'F', 'Lax')
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 30, 'M', '')
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 30, 'M', 'Bashful')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 14, 'F')
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 26, 'M')
